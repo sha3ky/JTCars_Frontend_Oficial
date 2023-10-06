68fcc837e789ad8524bda2cea30bb1a6cc2b39a5
sha3ky <116161880+sha3ky@users.noreply.github.com>
2023-10-06 17:45:45 +0200

    Update AdminPart.vue

--- src/components/AdminPart.vue
+++ src/components/AdminPart.vue
@@ -235,7 +235,7 @@ export default defineComponent({
    },
 
    setup(props) {
-      const apiUrlWeb = "https://05e7-37-158-136-4.ngrok-free.app/"
+      const apiUrlWeb = "https://9b1d-37-158-136-4.ngrok-free.app/"
       const api="http://127.0.0.1:8000/"
       const tab = ref("general");
       const group = ref(["1"]);
