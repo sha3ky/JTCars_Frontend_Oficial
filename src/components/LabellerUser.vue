5f2b5258ae904a13811c18618fb07585e687f864
sha3ky <116161880+sha3ky@users.noreply.github.com>
2023-10-06 12:24:37 +0200

    Update LabellerUser.vue

--- src/components/LabellerUser.vue
+++ src/components/LabellerUser.vue
@@ -91,7 +91,7 @@ export default defineComponent({
       userIsAdmin: Boolean,
    },
    setup(props) {
-      const apiUrlWeb = "https://b2e0-37-158-136-4.ngrok-free.app/"
+      const apiUrlWeb = "https://f06d-37-158-136-4.ngrok-free.app/"
       const api="http://127.0.0.1:8000/"
       let imagenBBDD;
       const textName = ref("");
