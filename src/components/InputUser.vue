11df25ef06d7eee9537179b809180c664b094a49
sha3ky <116161880+sha3ky@users.noreply.github.com>
2023-10-06 11:29:36 +0200

    Update InputUser.vue

--- src/components/InputUser.vue
+++ src/components/InputUser.vue
@@ -70,7 +70,7 @@ export default defineComponent({
    },
    data() {
       return {
-         apiUrlWeb: "http://68fa-37-158-136-4.ngrok-free.app/",
+         apiUrlWeb: "https://68fa-37-158-136-4.ngrok-free.app/",
          api: "http://127.0.0.1:8000/",
          correoInput: "", // Define correoInput data property
          contrasenaInput: "", // Define contrasenaInput data property
