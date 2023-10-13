c77b1152ee4fccc8b7cc968acbe2019f88b4ec48
sha3ky <116161880+sha3ky@users.noreply.github.com>
2023-10-13 10:53:59 +0200

    Update apiLink.js

--- src/composable/apiLink.js
+++ src/composable/apiLink.js
@@ -1,3 +1,3 @@
 //const apiLink = "http://127.0.0.1:8000/";
-const apiLink = "https://3cbf-37-158-178-140.ngrok-free.app/";
+const apiLink = " https://aa61-31-221-221-222.ngrok-free.app/";
 export default apiLink;
