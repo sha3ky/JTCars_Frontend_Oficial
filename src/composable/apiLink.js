d724e55c84cbd32678c4645010a78c5a6fdf6362
sha3ky <sha3ky@gmail.com>
2023-10-14 16:30:20 +0200

    cooregido apiLink

--- src/composable/apiLink.js
+++ src/composable/apiLink.js
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 const apiLink = "http://127.0.0.1:8000/";
-// const apiLink = "https://33e1-2-142-34-109.ngrok-free.app/";
-=======
-//const apiLink = "http://127.0.0.1:8000/";
-const apiLink = " https://aa61-31-221-221-222.ngrok-free.app/";
->>>>>>> b3b0ca53488c1c08cf478f1ae9f2528e2eb8793a
 export default apiLink;
