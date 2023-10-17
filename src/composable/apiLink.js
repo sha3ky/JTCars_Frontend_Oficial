32f8e3462c309fe6b3a559c2a0d9e74a3d773a6a
sha3ky <sha3ky@gmail.com>
2023-10-17 08:46:00 +0200

    faltaba algo

--- src/composable/apiLink.js
+++ src/composable/apiLink.js
@@ -1,3 +1,3 @@
 const apiLink =
-   "app-ff4d091f-a880-4586-ba0e-84edc6d9dac2-do-user-14812175-0.b.db.ondigitalocean.com";
+   "app-ff4d091f-a880-4586-ba0e-84edc6d9dac2-do-user-14812175-0.b.db.ondigitalocean.com/";
 export default apiLink;
