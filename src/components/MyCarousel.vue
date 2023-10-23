250ca225a815dd663ac1780d95872b278bd9b8ac
sha3ky <sha3ky@gmail.com>
2023-10-23 11:16:43 +0200

    parte front casi finish

---
+++ src/components/MyCarousel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="q-pa-md" style="width: 100%;">
+    <q-carousel
+      swipeable
+      animated
+      v-model="slide"
+      thumbnails
+      infinite
+      arrows
+
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+      <q-carousel-slide :name="5" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="6" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="7" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+      <q-carousel-slide :name="8" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+    </q-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slide: 1,
+
+    };
+  },
+};
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -25,13 +25,13 @@
                   </q-list>
                </q-menu>
             </q-btn>
-            <!-- <q-img
-               height="40px"
+            <q-img
+               height="70px"
                width="108px"
-               src="../../public/logoIconCar.png"
+               src="../../public/benysCarlogoMetal.png"
                v-if="$q.screen.width > 600"
             >
-            </q-img> -->
+            </q-img>
             <q-separator
                vertical
                inset
@@ -73,88 +73,140 @@
                ></q-btn>
             </div>
             <q-toggle
-               color="white"
+               color="red"
                dark
-               v-model="white"
+               v-model="toggleDark"
                @click="toggleDarkMode"
             />
          </q-toolbar>
       </q-header>
       <q-footer elevated class="bg-blue-grey-9">
          <q-toolbar>
-            <q-toolbar-title>Footer</q-toolbar-title>
+            <q-toolbar-title style="text-align: center;">Made with love by Sha3ky's TEAM</q-toolbar-title>
          </q-toolbar>
       </q-footer>
 
       <q-page-container style="min-height: 100vh; text-align: center">
+         <div>
+            <div style="padding: 25px; background: #37474f">
+               <p
+                  style="
+                     font-size: 5vw;
+                     text-align: center;
+                     text-align: center;
+                     color: white;
+                  "
+               >
+                  Coches de segunda mano y vehículos de ocasión.
+               </p>
+               <!-- <div>
+                  <q-img
+                     src="/benysCarlogoMetal.png"
+                     width="200"
+                     height="200"
+                     alt="Beny1 Logo"
+                  ></q-img>
+               </div> -->
+            </div>
+         </div>
          <q-img
             src="/banner.png"
             class="q-mx-auto"
             :max-width="400"
             :max-height="425"
             alt="Beny1 Logo"
-
          ></q-img>
          <div>
-            <div style="padding: 25px;    background: #37474F;">
-               <p style="font-size:6vw;text-align: center;    text-align: center;
-    color: white;">
-                  Coches de segunda mano y vehículos de ocasión.
-               </p>
+            <div class="q-pa-md row items-start q-gutter-md">
+               <div class="row">
+                  <q-card
+                     v-for="(image, index) in imageUrls"
+                     :key="index"
+                     style="padding: 10px"
+                     flat
+                     bordered
+                     class="my-card col-6"
+                  >
+
+                     <q-img :src="image">
+                        <div
+                           style="
+                              display: flex;
+                              justify-content: flex-end;
+                              display: contents;
+                           "
+                        >
+                        <div>
+                          <q-badge rounded color="red" label="DESCUENTO" />
+                          <q-badge rounded color="green" label="OFERTA" />
+                          <q-badge rounded color="blue" label="HIBRIDO" />
+                        </div>
+                           <div
+                              style="display: flex; justify-content: flex-end"
+                           >
+                              <q-img
+                                 height="6vw"
+                                 width="10vw"
+                                 src="../../public/benysCarlogoMetal.png"
+                              >
+                              </q-img>
+                           </div>
+                        </div>
+                     </q-img>
+
+                     <q-card-section>
+                        <div class="text-overline text-orange-9">Precio</div>
+                        <div class="text-h5 q-mt-sm q-mb-xs">2500$</div>
+                        <div>
+                           <div class="text-caption text-grey">
+                              <div>Año de fabricación: 1998</div>
+                              <div>Kilómetros: 220000</div>
+                              <div>Etiqueta: C</div>
+                           </div>
+                        </div>
+                     </q-card-section>
+
+                     <q-card-actions>
+                        <q-btn
+                           flat
+                           color="primary"
+                           label="Más Fotos"
+                           @click="carouselFoto"
+                        />
+                        <q-btn
+                           flat
+                           color="secondary"
+                           label="Datos Adicionales"
+                        />
+
+                        <q-space />
+
+                        <q-btn
+                           color="grey"
+                           round
+                           flat
+                           dense
+                           :icon="
+                              expanded
+                                 ? 'keyboard_arrow_up'
+                                 : 'keyboard_arrow_down'
+                           "
+                           @click="expanded = !expanded"
+                        />
+                     </q-card-actions>
+
+                     <q-slide-transition>
+                        <div v-show="expanded">
+                           <q-separator />
+                           <q-card-section class="text-subtitle2">
+                              {{ lorem }}
+                           </q-card-section>
+                        </div>
+                     </q-slide-transition>
+                  </q-card>
+               </div>
             </div>
          </div>
-
-         <div>
-            <div class="q-pa-md row items-start q-gutter-md" >
-              <div class="row">
-              <q-card
-                v-for="(image, index) in imageUrls"
-                :key="index"
-                style="padding: 10px;"
-                flat
-                bordered
-                class="my-card col-6"
-
-              >
-                <q-img :src="image" />
-
-                <q-card-section>
-                  <div class="text-overline text-orange-9">Overline</div>
-                  <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
-                  <div class="text-caption text-grey">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  </div>
-                </q-card-section>
-
-                <q-card-actions>
-                  <q-btn flat color="primary" label="Share"  @click="carouselFoto"/>
-                  <q-btn flat color="secondary" label="Book" />
-
-                  <q-space />
-
-                  <q-btn
-                    color="grey"
-                    round
-                    flat
-                    dense
-                    :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-                    @click="expanded = !expanded"
-                  />
-                </q-card-actions>
-
-                <q-slide-transition>
-                  <div v-show="expanded">
-                    <q-separator />
-                    <q-card-section class="text-subtitle2">
-                      {{ lorem }}
-                    </q-card-section>
-                  </div>
-                </q-slide-transition>
-              </q-card>
-            </div>
-          </div>
-          </div>
-
 
          <div style="text-align: center; display: inline-block">
             <InputUser :show-newUser="showNewUser" />
@@ -186,6 +238,10 @@
             />
          </q-page> -->
          <router-view />
+         <q-dialog v-model="dialogVisible">
+            <MyCarousel />
+            <!-- Include your custom carousel component here -->
+         </q-dialog>
       </q-page-container>
    </q-layout>
 </template>
@@ -196,7 +252,7 @@ import { useQuasar } from "quasar";
 
 import InputUser from "components/InputUser.vue"; // Replace with the actual path
 import loginUser from "src/components/loginUser.vue";
-import AdminPart from "components/AdminPart.vue"; // Adjust the path as needed
+import MyCarousel from "src/components//MyCarousel.vue"; // Adjust the path as needed
 import LabellerUser from "components/LabellerUser.vue";
 
 //import EssentialLink from 'components/EssentialLink.vue'; // Adjust the path as needed
@@ -209,7 +265,7 @@ export default defineComponent({
          showLoginUser: false,
          userId: null,
          userIsAdmin: false,
-         white: ref(false),
+         toggleDark: ref(false),
          modelSelectedMenu: ref("coches"),
          optionsMenu: [
             { label: "Coches", value: "coches" },
@@ -217,18 +273,17 @@ export default defineComponent({
             { label: "Contacto", value: "contacto" },
          ],
          imageUrls: [
-        "https://cdn.quasar.dev/img/parallax1.jpg",
-        "https://cdn.quasar.dev/img/parallax2.jpg",
-        "https://cdn.quasar.dev/img/parallax1.jpg",
-        "https://cdn.quasar.dev/img/parallax2.jpg",
-        "https://cdn.quasar.dev/img/parallax1.jpg",
-        "https://cdn.quasar.dev/img/parallax2.jpg",
-        "https://cdn.quasar.dev/img/parallax1.jpg",
-        "https://cdn.quasar.dev/img/parallax2.jpg"
-      ],
-      expanded: false,
-      lorem:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            "https://cdn.quasar.dev/img/parallax1.jpg",
+            "https://cdn.quasar.dev/img/parallax2.jpg",
+            "https://cdn.quasar.dev/img/parallax1.jpg",
+            "https://cdn.quasar.dev/img/parallax2.jpg",
+            "https://cdn.quasar.dev/img/parallax1.jpg",
+            "https://cdn.quasar.dev/img/parallax2.jpg",
+            "https://cdn.quasar.dev/img/parallax1.jpg",
+            "https://cdn.quasar.dev/img/parallax2.jpg",
+         ],
+         expanded: false,
+         lorem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
          // Initialize with your desired value
       };
    },
@@ -264,6 +319,7 @@ export default defineComponent({
       //EssentialLink,
       InputUser,
       loginUser,
+      MyCarousel,
       // AdminPart,
       // LabellerUser,
    },
@@ -273,20 +329,27 @@ export default defineComponent({
       $q.dark.set(true); // or false or "auto"
       $q.dark.toggle(); // toggle
 
+      const dialogVisible = ref(false);
+      function carouselFoto() {
+         dialogVisible.value = true;
+      }
 
-      function carouselFoto () {
-      $q.dialog({
-        title: 'Alert',
-        message: 'Some message'
-      }).onOk(() => {
-        // console.log('OK')
-      }).onCancel(() => {
-        // console.log('Cancel')
-      }).onDismiss(() => {
-        // console.log('I am triggered on both OK and Cancel')
-      })
-    }
-    return { carouselFoto }
+      // function carouselFoto() {
+      //    $q.dialog({
+      //       title: "Alert",
+      //       message: "Some message",
+      //    })
+      //       .onOk(() => {
+      //          // console.log('OK')
+      //       })
+      //       .onCancel(() => {
+      //          // console.log('Cancel')
+      //       })
+      //       .onDismiss(() => {
+      //          // console.log('I am triggered on both OK and Cancel')
+      //       });
+      // }
+      return { carouselFoto, dialogVisible };
    },
 });
 </script>
