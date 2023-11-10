288f6776b98331f95eadf256a9f1262f118def36
sha3ky <sha3ky@gmail.com>
2023-11-10 15:38:18 +0100

    la barrita

--- src/composable/apiLink.js
+++ src/composable/apiLink.js
@@ -1,3 +1,3 @@
 // const apiLink = "http://127.0.0.1:8000/";
-const apiLink="app-ff4d091f-a880-4586-ba0e-84edc6d9dac2-do-user-14812175-0.b.db.ondigitalocean.com"
+const apiLink="app-ff4d091f-a880-4586-ba0e-84edc6d9dac2-do-user-14812175-0.b.db.ondigitalocean.com/"
 export default apiLink;
