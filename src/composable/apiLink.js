b1bda2466da1a99d69b1cbeb01fea5d95bf5f3e1
sha3ky <sha3ky@gmail.com>
2026-09-18 06:52:52 +0200

    api lonk oficial rayman

--- src/composable/apiLink.js
+++ src/composable/apiLink.js
@@ -1,4 +1,4 @@
-const apiLink = "http://127.0.0.1:8000/";
-// const apiLink = "https://jtcars.raymansoftware.com/";
+// const apiLink = "http://127.0.0.1:8000/";
+const apiLink = "https://jtcars.raymansoftware.com/";
 
 export default apiLink;
